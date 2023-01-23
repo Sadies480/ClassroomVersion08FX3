1ae653739a6a0264f329d1c2b798a2f334f46d06
Sadie Velayutham <sadieshero@gmail.com>
2023-01-23 20:03:09 +0100

    Reformated and Doc

--- Doc/package-search-index.js
+++
@@ -1,5 +0,0 @@
-packageSearchIndex = [{
-    "l": "Alle Packages",
-    "u": "allpackages-index.html"
-}, {"l": "com.classroom.classroomversion08fx"}, {"l": "com.classroom.classroomversion08fx.gui"}, {"l": "com.classroom.classroomversion08fx.io"}, {"l": "com.classroom.classroomversion08fx.logic"}];
-updateSearchResults();
